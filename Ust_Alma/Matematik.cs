fa2ea025babd4f762e950299da1fd8ea7ca2b9b2
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:43:40 +0300

    Proje dosyası ekle.

---
+++ Ust_Alma/Matematik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ust_Alma
+{
+    public class Matematik
+    {
+        /// <summary>
+        /// Parametre olarak aldığı taban ve kuvvet bağlı olarak ustAlma işemi gerçekleştirir.
+        /// </summary>
+        /// <param name="taban">Taban</param>
+        /// <param name="kuvvet">Kuvvet</param>
+        /// <returns>Sonuç</returns>
+        public static double UstAlma(double taban, double kuvvet)
+        {
+            double s = 1;
+            for (int i = 1; i <= kuvvet; i++)
+                s *= taban;
+            return s;
+        }
+
+      
+    }
+}
---
+++ Ust_Alma/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ust_Alma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1 = Convert.ToDouble(Console.ReadLine());
+            double s2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0}^{1}={2}",s1,s2,Matematik.UstAlma(s1,s2));  // s1 s2 sayılarını girerek üstlerini alabilirsin.
+        }
+    }
+}
